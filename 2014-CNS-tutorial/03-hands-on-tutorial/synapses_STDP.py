0eb0f04f5175a8a2a16484b3f1b9e9d4a9c57ba0
Marcel Stimberg <marcel.stimberg@inserm.fr>
2015-06-30 15:02:03 +0200

    Replace synapses_STDP.py by an actual STDP example Closes #1

--- 2014-CNS-tutorial/03-hands-on-tutorial/synapses_STDP.py
+++ 2014-CNS-tutorial/03-hands-on-tutorial/synapses_STDP.py
@@ -1,3 +1,9 @@
+'''
+Spike-timing dependent plasticity
+Adapted from Song, Miller and Abbott (2000) and Song and Abbott (2001)
+
+This simulation takes a long time!
+'''
 from time import time
 from brian2 import *
 
@@ -16,38 +22,32 @@ dApre   = .01
 dApost  = -dApre * taupre / taupost * 1.05
 dApost *= gmax
 dApre  *= gmax
-tau_homeo = 1*second
 
 eqs_neurons = '''
 dv/dt=(ge*(Ee-vr)+El-v)/taum : volt   # the synaptic current is linearized
 dge/dt=-ge/taue : 1
-drate/dt = -rate/tau_homeo : 1
 '''
 
 input = PoissonGroup(N, rates=F)
-neurons = NeuronGroup(1, eqs_neurons, threshold='v>vt', reset='v=vr; rate += 1')
+neurons = NeuronGroup(1, eqs_neurons, threshold='v>vt', reset='v=vr')
 S = Synapses(input, neurons,
-             '''dw/dt = 0.01*w*(10 - rate_post): 1''',
-             pre = '''ge += w''', connect=True)
-
+             '''w:1
+                dApre/dt=-Apre/taupre    : 1 (event-driven)
+                dApost/dt=-Apost/taupost : 1 (event-driven)''',
+             pre='''ge+=w
+                    Apre+=dApre
+                    w=clip(w+Apost,0,gmax)''',
+             post='''Apost+=dApost
+                     w=clip(w+Apre,0,gmax)''',
+             connect=True,
+             )
 S.w='rand()*gmax'
-dmax = 5*ms
-S.delay = 'rand()*dmax'
 start_time = time()
-
-w_recorder = StateMonitor(S, 'w', record=True, when=Clock(dt=1*second))
-rate_recorder = StateMonitor(neurons, 'rate', record=True)
-
-run(10 * second, report='text')
+run(100 * second, report='text')
 print "Simulation time:", time() - start_time
 
-subplot(411)
+subplot(211)
 plot(S.w[:] / gmax, '.')
-subplot(412)
+subplot(212)
 hist(S.w[:] / gmax, 20)
-subplot(413)
-imshow(numpy.diff(w_recorder.w, 1), aspect='auto', interpolation='nearest')
-colorbar()
-subplot(414)
-plot(rate_recorder.rate[0])
 show()
